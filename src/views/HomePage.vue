65afe4c03d3aed61414402de8b86624db51b126d
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2023-12-05 11:00:29 -0300

    Adding HomePage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <the-header></the-header>
+    <div class="kanban-board">
+      <task-list :tasks="activeTasks" title="Ativas"></task-list>
+      <task-list :tasks="ongoingTasks" title="Em Andamento"></task-list>
+      <task-list :tasks="completedTasks" title="Concluídas"></task-list>
+    </div>
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/shared/TheHeader.vue";
+import TheFooter from "@/components/shared/TheFooter.vue";
+import TaskList from "@/components/kanban/TaskList.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    TheHeader,
+    TheFooter,
+    TaskList,
+  },
+  data() {
+    return {
+      activeTasks: [],
+      ongoingTasks: [],
+      completedTasks: [],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  /* Estilos para a HomePage */
+}
+.kanban-board {
+  display: flex;
+  justify-content: space-around;
+  /* Estilos para o quadro Kanban */
+}
+</style>
