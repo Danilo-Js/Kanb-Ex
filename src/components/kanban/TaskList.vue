44e004aaf9d5f4bd386d62cf11e6a84eb3afca56
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2023-12-05 10:52:27 -0300

    Adding TaskList

---
+++ src/components/kanban/TaskList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="task-list">
+    <h2>{{ title }}</h2>
+    <div v-for="task in tasks" :key="task.id">
+      <task-card :task="task" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskCard from "./TaskCard.vue";
+
+export default {
+  name: "TaskList",
+  components: {
+    TaskCard,
+  },
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  // Métodos para manipulação do TaskList ficarão aqui
+};
+</script>
+
+<style scoped>
+.task-list {
+  margin: 20px;
+}
+</style>
