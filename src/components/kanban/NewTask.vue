a8701ce1dca3a2d483384ebb00c99d03ffd92bcf
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2023-12-11 20:13:16 -0300

    Fix NewTask Style

--- src/components/kanban/NewTask.vue
+++ src/components/kanban/NewTask.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="new-task">
-    <p></p>
-    Nome Da Tarefa: <input v-model="name" type="text" /> Responsável:
-    <input v-model="responsible" type="text" /> Descrição:
-    <input v-model="description" type="text" />
-    <p></p>
-    Prazo Máximo: <input v-model="deadline" type="text" /> Prazo Estimado:
-    <input v-model="estimatedTime" type="text" />
-    <p></p>
-    <button @click="add()">Adicionar</button>
+  <div>
+    <button v-if="!showForm" @click="toggleForm">Adicionar Tarefa</button>
+    <div v-if="showForm" class="new-task">
+      Nome da tarefa <input v-model="name" type="text" /> Responsável
+      <input v-model="responsible" type="text" /> Descrição
+      <input v-model="description" type="text" /> Prazo máximo
+      <input v-model="deadline" type="text" /> Prazo estimado
+      <input v-model="estimatedTime" type="text" />
+      <button @click="add">Adicionar</button>
+    </div>
   </div>
 </template>
 
@@ -16,6 +16,7 @@
 export default {
   data() {
     return {
+      showForm: false,
       name: "",
       responsible: "",
       description: "",
@@ -25,6 +26,9 @@ export default {
     };
   },
   methods: {
+    toggleForm() {
+      this.showForm = !this.showForm;
+    },
     add() {
       if (
         this.name === "" ||
@@ -54,6 +58,7 @@ export default {
           (this.deadline = ""),
           (this.estimatedTime = "");
       }
+      this.toggleForm();
     },
   },
 };
@@ -61,10 +66,64 @@ export default {
 
 <style scoped>
 .new-task {
-  align-content: center;
-  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
   border-radius: 10px;
-  background-color: gray;
-  color: black;
+  background-color: #f5f5f5;
+  color: #333;
+  max-width: 500px;
+  margin: 20px auto;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.new-task input[type="text"] {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+.new-task button {
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.new-task button:hover {
+  background-color: #333;
+}
+
+.new-task label {
+  margin-top: 10px;
+  font-weight: bold;
+}
+
+.new-task p {
+  display: none;
+}
+
+button {
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: block;
+  width: 100%;
+}
+
+button:hover {
+  background-color: #333;
 }
 </style>
